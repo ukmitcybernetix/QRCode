f0985662b621086405b0086f9e01627afc74a3f0
Fattahul Akbar <akbarfattahul22@gmail.com>
2019-04-29 22:32:58 +0700

    Add Ticket Result and Fix ScanActivity

--- app/src/main/java/com/zendev/qrcode/ScanActivity.java
+++ app/src/main/java/com/zendev/qrcode/ScanActivity.java
@@ -1,13 +1,68 @@
 package com.zendev.qrcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class ScanActivity extends AppCompatActivity {
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import info.androidhive.barcode.BarcodeReader;
+
+public class ScanActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
+
+    BarcodeReader barcodeReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_scanner);
+
+    }
+
+    @Override
+    public void onScanned(Barcode barcode) {
+        barcodeReader.playBeep();
+
+        Intent intent = new Intent(ScanActivity.this, TicketResultActivity.class);
+        intent.putExtra("code", barcode.displayValue);
+
+        startActivity(intent);
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> list) {
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String s) {
+        Toast.makeText(getApplicationContext(), "Error occurred while scanning " + s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCameraPermissionDenied() {
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/zendev/qrcode/TicketResultActivity.java
@@ -0,0 +1,13 @@
+package com.zendev.qrcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TicketResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_result);
+    }
+}
